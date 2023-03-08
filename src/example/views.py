f6ff10a606e932c08e7fb3b991a2c40cccf0f4fd
Juan Felipe <juan.fll@hotmail.com>
2023-03-08 20:54:39 -0300

    implementado user_logout e my_login

--- src/example/views.py
+++ src/example/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import auth
 from django.shortcuts import redirect, render
 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm
 from .models import Profile
 
 
@@ -14,18 +16,30 @@ def register(request):
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            current_user = form.save(commit=False)
-            form.save()
-            profile = Profile.objects.create(user=current_user)
-            return redirect('my-login')
+            return redirect('example:my-login')
     return render(request, 'example/register.html', {'form': form})
 
 def my_login(request):
-    return render(request, 'example/mylogin.html', {})
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect("example:dashboard")
+    
+    return render(request, 'example/mylogin.html', {'form': form})
 
 def my_dashboard(request):
     return render(request, 'example/dashboard.html', {})
 
 def profile_management(request):
     return render(request, 'example/profile-management.html', {})
-    
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect("homepage")
