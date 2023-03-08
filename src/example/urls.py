7e6b63a7d0daed641b1dadbd6d1a0992fea5d06f
Juan Felipe <juan.fll@hotmail.com>
2023-03-08 20:25:30 -0300

    incluindo app_name ao urls.py

--- src/example/urls.py
+++ src/example/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name='example'
+
 urlpatterns = [ 
-    path('', views.index, name=""),
-    path('register', views.register, name = "register"),
-    path('my-login', views.my_login, name = "my-login"),
-    path('dashboard', views.my_dashboard, name = "my-dashboard"),
-    path('profile-management', views.profile_management, name = "profile-management"),
+    path('', views.index, name="homepage"),
+    path('register', views.register, name="register"),
+    path('my-login', views.my_login, name="my-login"),
+    path('dashboard', views.my_dashboard, name="my-dashboard"),
+    path('profile-management', views.profile_management, name="profile-management"),
 
 ]
