f8391588b3024f2201e8ac400e05c7a0a5f7f906
Juan Felipe <juan.fll@hotmail.com>
2023-03-09 20:08:32 -0300

    adicionada logica atualização na função profile_management e de deletarconta na função delete_conta e decorator de loginrequired nas views que precisam

--- src/example/views.py
+++ src/example/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, auth
 from django.shortcuts import redirect, render
 
-from .forms import CreateUserForm, LoginForm
+from .forms import CreateUserForm, LoginForm, UpdateUserForm
 from .models import Profile
 
 
@@ -33,13 +34,30 @@ def my_login(request):
                 return redirect("example:dashboard")
     
     return render(request, 'example/mylogin.html', {'form': form})
-
+@login_required(login_url="example:my-login")
 def my_dashboard(request):
     return render(request, 'example/dashboard.html', {})
 
+@login_required(login_url="example:my-login")
 def profile_management(request):
-    return render(request, 'example/profile-management.html', {})
+    form = UpdateUserForm(instance=request.user)
+    if request.method == 'POST':
+        form = UpdateUserForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('example:dashboard')
+
+    return render(request, 'example/profile-management.html', {'form': form})
 
 def user_logout(request):
     auth.logout(request)
-    return redirect("homepage")
+    return redirect("example:homepage")
+
+@login_required(login_url="example:my-login")
+def delete_conta(request):
+    if request.method == "POST":
+        deleteUser  = User.objects.get(username=request.user)
+        deleteUser.delete()
+        return redirect("example:homepage")
+    
+    return render(request, "example/deleteconta.html", {})
