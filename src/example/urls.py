7476da782abe033d8d999df5569fe36795be0b59
Juan Felipe <juan.fll@hotmail.com>
2023-03-09 20:09:02 -0300

    adicionado path de delete-conta

--- src/example/urls.py
+++ src/example/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('user-logout', views.user_logout, name='user-logout'),
     path('dashboard', views.my_dashboard, name="dashboard"),
     path('profile-management', views.profile_management, name="profile-management"),
+    path('delete-conta', views.delete_conta, name='delete-conta')
 ]
