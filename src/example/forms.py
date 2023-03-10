892a224541916c34beab2fa26b90d518d611fbfb
Juan Felipe <juan.fll@hotmail.com>
2023-03-09 22:34:29 -0300

    criando updateprofileform para alterar foto do usuario

--- src/example/forms.py
+++ src/example/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.forms.widgets import PasswordInput, TextInput
 
+from .models import Profile
+
 
 #user registration
 class CreateUserForm(UserCreationForm):
@@ -22,3 +24,10 @@ class UpdateUserForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
         exclude = ['password1', 'password2']
+
+class UpdateProfileForm(forms.ModelForm):
+    profile_pic = forms.ImageField(widget=forms.FileInput(attrs={'class':'form-control-file'}))
+    class Meta:
+        model = Profile
+        fields =  ['profile_pic']
+      
