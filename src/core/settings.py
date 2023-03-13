ba80cb0b16313e9e0ec195a00166ea7c194d3d0e
Juan Felipe <juan.fll@hotmail.com>
2023-03-13 19:16:49 -0300

    adicionando configurações do AWS SES

--- src/core/settings.py
+++ src/core/settings.py
@@ -144,3 +144,12 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_FILE_OVERWRITE=False
+
+# EMAIL CONFIGURATIONS
+
+EMAIL_BACKEND = 'django_ses.SESBackend'
+
+
+AWS_SES_REGION_NAME = "sa-east-1"
+
+AWS_SES_REGION_ENDPOINT = f"email.{AWS_SES_REGION_NAME}.amazonaws.com"
