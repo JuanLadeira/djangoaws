8031b582f618c02db74f49bb5c8a3e4c1e6a6aa2
Juan Felipe <juan.fll@hotmail.com>
2023-03-13 19:41:13 -0300

    configurando elasticbeanstalk

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,6 +25,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+# CSRF_TRUSTED_ORIGIN = ['']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
